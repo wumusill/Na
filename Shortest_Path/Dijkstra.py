8fa4d272928805f709591701d9a22cba2963e592
JaHyeon_Gu <jahyungu@naver.com>
2022-04-08 21:52:12 +0900

    create Shortest_Path dir

---
+++ Shortest_Path/Dijkstra.py
@@ -0,0 +1,80 @@
+# 1. 출발 노드를 설정한다.
+# 2. 최단 거리 테이블을 초기화한다.
+# 3. 방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선택한다.
+# 4. 해당 노드를 거쳐 다른 노드로 가는 비용을 계산하여 최단 거리 테이블을 갱신한다.
+# 5. 위 과정에서 3과 4번을 반복한다.
+
+# 다익스트라 알고리즘은 최단 경로를 구하는 과정에서 각 노드에 대한 현재까지의 최단 거리 정보를
+# 항상 1차원 리스트에 저장하며 리스트를 계속 갱신한다는 특징이 있다.
+
+# 다익스트라 알고리즘을 구현하는 방법은 2가지이다.
+# 1. 구현하기 쉽지만 느리게 동작하는 코드
+# 2. 구현하기에 조금 더 까다롭지만 빠르게 동작하는 코드
+
+
+# 1. 간단한 다익스트라 알고리즘
+# O(V^2)의 시간 복잡도
+# 각 노드에 대한 최단 거리를 담는 1차원 리스트를 선언한다.
+# 단계마다 방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선택하기 위해 매 단계마다 1차원 리스트의 모든 원소를 확인(순차 탐색)한다.
+
+import sys
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
+
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0   # 가장 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n - 1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+# 다익스트르 알고리즘 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i])
